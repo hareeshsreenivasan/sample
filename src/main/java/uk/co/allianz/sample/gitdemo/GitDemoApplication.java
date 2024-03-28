cd760641768dbb2c2f171f27b6b2cbc7f382c7fc
Hareesh Sreenivasan <trivandrum50@gmail.com>
2024-03-28 09:06:01 +0530

    first commit

---
+++ src/main/java/uk/co/allianz/sample/gitdemo/GitDemoApplication.java
@@ -0,0 +1,13 @@
+package uk.co.allianz.sample.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/allianz/sample/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.allianz.sample.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
